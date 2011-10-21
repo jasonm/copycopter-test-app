c5ee817eeff97d0f5d49d30b96ba82a87cfec8b4
Jason Morrison <jmorrison@thoughtbot.com>
2011-10-21 09:51:30 -0400

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copyland::Application.config.secret_token = '0551180131f8b58e8ebfa87b781fa3f704080b5c01f6c46661bdc28bff7a8e5766b2cbbb67ff71e9d4ea749f1f64e2fbb1a279266bc228b1e513601d1250d808'
