44cc315085982a0da735fa14938866a781551d60
Jason Morrison <jmorrison@thoughtbot.com>
2011-10-21 16:19:23 -0400

    Add new CC-staging key for https://copycopter-staging.heroku.com/accounts/jmorrison-sslocales2df141/projects/copyland

--- config/initializers/copycopter.rb
+++ config/initializers/copycopter.rb
@@ -1,5 +1,5 @@
 CopycopterClient.configure do |config|
-  config.api_key = '5aff86ad127e87147f1d3c10d29db3b4'
+  config.api_key = 'fd4b00b979045f24e9d4543fd570a878'
   config.host = 'copycopter-staging.heroku.com'
   config.secure = false
 end
